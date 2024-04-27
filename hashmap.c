1a43ea88532f190cc716c70470016390fd5c4ee0
Alexx525 <163464627+Alexx525@users.noreply.github.com>
2024-04-27 03:00:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,10 +41,10 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value)
 {
-  
+  if (map == NULL || key == NULL)
+    return;
 
-  if ((map->size + 1) >= (map->capacity * 0.7))
-    enlarge(map);
+  
 
   long posicion = hash(key, map->capacity);
   Pair * pair = searchMap(map, key);
