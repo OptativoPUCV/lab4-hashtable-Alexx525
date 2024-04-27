dac6148c2aa54b2a563ff47a61d0bdcaa59cd7a4
Alexx525 <163464627+Alexx525@users.noreply.github.com>
2024-04-27 02:37:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -92,13 +92,13 @@ void enlarge(HashMap * map)
 
   map->size = 0;
 
-  for (long i = 0; i < (map->capacity / 2); i++)
+  /*for (long i = 0; i < (map->capacity / 2); i++)
   {
     if (map->buckets[i] != NULL)
     {
       eraseMap(map, map->buckets[i]->key);
     }
-  }
+  }*/
   
   for (long i = 0; i < (map->capacity / 2); i++)
   {
