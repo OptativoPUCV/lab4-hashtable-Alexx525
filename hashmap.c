bba1a3d6f6224449d3d6883bfecea636e55e9090
Alexx525 <163464627+Alexx525@users.noreply.github.com>
2024-04-27 02:57:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,13 +52,17 @@ void insertMap(HashMap * map, char * key, void * value)
 
   if (pair == NULL)
   {
-    while (map->buckets[posicion] != NULL && map->buckets[posicion]->key != 0)
+    while (posicion < map->capacity)
+    {
+      if (map->buckets[posicion] == NULL)
+      {
+        map->buckets[posicion] = createPair(key, value);
+        map->current = posicion;
+        map->size = map->size + 1;
+        return;
+      }
       posicion = (posicion + 1) % map->capacity;
-
-    map->buckets[posicion] = createPair(key, value);
-    map->current = posicion;
-    map->size = map->size + 1;
-    return;
+    }
   }
 
   else
